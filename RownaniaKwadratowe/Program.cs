b94291571cb8549e4cd6a384d04d4688407c54f9
Student <student@student.polsl.pl>
2018-03-12 09:17:03 +0100

    Rownanie liniowe

--- RownaniaKwadratowe/Program.cs
+++ RownaniaKwadratowe/Program.cs
@@ -14,18 +14,25 @@ namespace RownaniaKwadratowe
             a = 1;
             b = 4;
             c = -5;
-            double delta = b * b - 4 * a * c;
-            if (delta >= 0)
-            {
-                double x1 = (-b - Math.Sqrt(delta)) / 2 * a;
-                double x2 = (-b + Math.Sqrt(delta)) / 2 * a;
-                Console.WriteLine("Pierwiastki: {0} i {1}", x1, x2);
-            }
-            else 
-            {
-                Console.WriteLine("Delta mniejsza od zera");
-            }
 
+            if (a == 0)
+            {
+                Console.WriteLine("Równanie liniowe, x={0}", -c / b);
+            }
+            else
+            {
+                double delta = b * b - 4 * a * c;
+                if (delta >= 0)
+                {
+                    double x1 = (-b - Math.Sqrt(delta)) / 2 * a;
+                    double x2 = (-b + Math.Sqrt(delta)) / 2 * a;
+                    Console.WriteLine("Pierwiastki: {0} i {1}", x1, x2);
+                }
+                else
+                {
+                    Console.WriteLine("Delta mniejsza od zera");
+                }
+            }
             Console.ReadKey();
         }
     }
