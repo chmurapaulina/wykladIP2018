d250f6c965f82f9c1c04a0f66e2273fb135a8522
Student <student@student.polsl.pl>
2018-03-12 09:06:59 +0100

    Wyświetlanie OK

--- RownaniaKwadratowe/Program.cs
+++ RownaniaKwadratowe/Program.cs
@@ -11,12 +11,14 @@ namespace RownaniaKwadratowe
         static void Main(string[] args)
         {
             double a, b, c;
-            a = 2;
-            b = 5;
-            c = 10;
+            a = 1;
+            b = 4;
+            c = -5;
             double delta = b * b - 4 * a * c;
             double x1 = (-b - Math.Sqrt(delta)) / 2 * a;
             double x2 = (-b + Math.Sqrt(delta)) / 2 * a;
+            Console.WriteLine("Pierwiastki: {0} i {1}", x1, x2);
+            Console.ReadKey();
         }
     }
 }
