a31ceda23b22efd39e0e20240c91aa3417ab81f9
Student <student@student.polsl.pl>
2018-03-12 09:33:12 +0100

    Zmiana 2

--- RownaniaKwadratowe/Program.cs
+++ RownaniaKwadratowe/Program.cs
@@ -11,9 +11,9 @@ namespace RownaniaKwadratowe
         static void Main(string[] args)
         {
             double a, b, c;
-            a = 1;
-            b = 4;
-            c = -5;
+            a = double.Parse(Console.ReadLine());
+            b = double.Parse(Console.ReadLine());
+            c = double.Parse(Console.ReadLine());
 
             if (a == 0)
             {
