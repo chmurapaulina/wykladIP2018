5572e1ad03a8ec84cd24763e9496a076f82793e8
Student <student@student.polsl.pl>
2018-03-12 09:36:45 +0100

    Poprawka

--- RownaniaKwadratowe/Program.cs
+++ RownaniaKwadratowe/Program.cs
@@ -25,8 +25,8 @@ namespace RownaniaKwadratowe
                 double delta = b * b - 4 * a * c;
                 if (delta >= 0)
                 {
-                    double x1 = (-b - Math.Sqrt(delta)) / 2 * a;
-                    double x2 = (-b + Math.Sqrt(delta)) / 2 * a;
+                    double x1 = (-b - Math.Sqrt(delta)) / (2 * a);
+                    double x2 = (-b + Math.Sqrt(delta)) / (2 * a);
                     Console.WriteLine("Pierwiastki: {0} i {1}", x1, x2);
                 }
                 else
