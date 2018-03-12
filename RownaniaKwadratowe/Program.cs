56f6e96959c5762db96a143c1d7ccb4933039e0f
Student <student@student.polsl.pl>
2018-03-12 09:04:20 +0100

    Wersja z zadeklarowanymi zmiennymi

--- RownaniaKwadratowe/Program.cs
+++ RownaniaKwadratowe/Program.cs
@@ -10,6 +10,13 @@ namespace RownaniaKwadratowe
     {
         static void Main(string[] args)
         {
+            double a, b, c;
+            a = 2;
+            b = 5;
+            c = 10;
+            double delta = b * b - 4 * a * c;
+            double x1 = (-b - Math.Sqrt(delta)) / 2 * a;
+            double x2 = (-b + Math.Sqrt(delta)) / 2 * a;
         }
     }
 }
